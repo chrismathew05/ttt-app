e6d3bb5d307346b7cb25384a84e931aa3978e95e
Chris Mathew <chrismathew05@gmail.com>
2022-10-23 12:54:44 -0400

    Connect/disconnect simply on page visit/leave

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
   const [socket, setSocket] = useState(null);
   const [connStatus, setConnStatus] = useState(0); // 0 = Disconnected, 1 = Pending, 2 = Connected
 
-  const connect = () => {
-    let socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-    setConnStatus(1);
-
-    socket.onopen = () => {
-      console.log("CONNECTED.");
-      setSocket(socket);
-      setConnStatus(2);
-    };
-  }
-
-  const disconnect = () => {
-    if (socket != null) {
-      socket.close();
+  useEffect(() => {
+    if (socket == null) {
+      let webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+      setSocket(webSocket)
       setConnStatus(1);
 
-      socket.onclose = () => {
-        console.log("DISCONNECTED.");
-        setSocket(null);
-        setConnStatus(0);
+      webSocket.onopen = () => {
+        let test = { "action": "getInfo" }
+        webSocket.send(JSON.stringify(test));
+      };
+
+      webSocket.onmessage = event => {
+        console.log(event.data);
+        console.log("CONNECTED");
+        setConnStatus(2);
       };
     }
-  }
+  }, [socket]);
 
   return (
     <div className="App">
-      <button onClick={connStatus === 0 ? connect : disconnect} disabled={connStatus === 1}>
-        {["Connect", "Pending", "Disconnect"][connStatus]}
-      </button>
+      <h1>Tic-Tac-Toe</h1>
     </div>
   );
 }
